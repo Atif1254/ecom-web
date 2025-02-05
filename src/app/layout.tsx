1228497a4f9d595355194f43ee0f4f79d6a51896
Atif ahmed <167696985+Atif1254@users.noreply.github.com>
2025-02-05 09:37:50 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Script from "next/script";
+
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 
